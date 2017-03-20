ffb753f9967f93c369a319ca8181dca57c6615cc
m-woo <mark.chi.woo@gmail.com>
2017-03-19 22:40:55 -0700

    redirect route after create plane

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -21,27 +21,33 @@ angular.module('FlyApp')
 		console.log('error', err);
 	})
 }])
-.controller('DeleteCtrl', ['$scope', '$stateParams', 'PlanesAPI', function($scope, $stateParams, PlanesAPI){
-	PlanesAPI.deletePlane($stateParams.id).then(function success(res){
-		console.log('success', res);
-		$scope.plane = res.data;
-	}, function error(err){
-		console.log('error', err);
-	})
+.controller('DeleteCtrl', ['$scope', '$stateParams', 'PlanesAPI', '$location', function($scope, $stateParams, PlanesAPI, $location){
+	PlanesAPI.deletePlane($stateParams.id)
+	$scope.delete = function() {
+		$location.path('/')
+	}
+	// .then(function success(res){
+	// 	console.log('success', res);
+	// 	$scope.plane = res.data;
+	// }, function error(err){
+	// 	console.log('error', err);
+	// })
 }])
-.controller('CreateCtrl', ['$scope', '$stateParams', 'PlanesAPI', function($scope, $stateParams, PlanesAPI){
+.controller('CreateCtrl', ['$scope', '$stateParams', 'PlanesAPI', '$location', function($scope, $stateParams, PlanesAPI, $location){
 	$scope.enter = function() {
+		$location.path('/allPlanes')
 		var plane = {
 			'manufacturer': this.manufacturer,
 			'model': this.model,
 			'engines': this.engines,
-			'image': this.image
+			'url': this.url
 		}
-		PlanesAPI.addPlane(plane).then(function success(res){
-			console.log('success', res);
-		}, function error(err){
-			console.log('error', err)
-		})
+		PlanesAPI.addPlane(plane)
+		// .then(function success(res){
+		// 	console.log('success', res);
+		// }, function error(err){
+		// 	console.log('error', err)
+		// })
 	}
 }])
 
